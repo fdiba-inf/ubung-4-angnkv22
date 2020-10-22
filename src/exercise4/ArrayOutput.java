77d7f9ac1e98744cb6817560c1162e9de6f58dfe
angnkv22 <angel.neikov@gmail.com>
2020-10-22 06:13:33 +0000

    scheiße

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 
   public class ArrayOutput {
 
-    public static void main(String[] args) {
+    public static void main(StringC args) {
         Scanner input = new Scanner(System.in);
         int count = input.nextInt();
-      
+       int[] array = new int[count];
+       for (int i = 0; i < array.lenght; i++){
+         array [i] = input.nextInt();
+        int space = 0;
+        for (int i = 0; i < array.lenght; i++){
+          for (int a = 0; a < space; a++){
+            System.out.print(" ");
+          }
+          space++;
+          System.out.println (array[i]);
+        }
+       }
+    }
+    }
--- src/exercise4/ArrayReversal.java
+++ src/exercise4/ArrayReversal.java
@@ -1,13 +1,16 @@
 package exercise4;
 import java.util.Arrays;
+import java.util.Scanner;
 public class ArrayReversal {
   public static void main (String[] args) {
-    int[] a = {A, B, I, D, F};
-    int[] b = new int(a.lenght);
-    for (int i = a.lenght - 1; i >= 0; i--) {
-      System.out.println (a[i]);
+    Scanner input = new Scanner(System.in);
+    char[] array = new char(arrSize);
+    char[] reverseArray = new char(arrSize);
+    for (char i = a.lenght - 1; i >= 0; i--) {
+      System.out.println (array[i]);
     }
-    String numbersAsString = Arrays.toString(numbers);
-    System.out.println ("Reversed Symbols: " + Arrays.toString(numbers));
+    String numbersAsString = Arrays.toString(array);
+    System.out.println ("Reversed Symbols: " + reverseArray);
   }
-}
+  }
+  
